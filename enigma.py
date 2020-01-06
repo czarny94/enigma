4c3370c2c6b09fcabcd378ea1da9c006e471de33
warlock <maciek.czarnota@gmail.com>
2020-01-06 16:43:16 +0100

    podstawowy szablon strony ze stopką i paskiem nawigacyjnym

--- enigma.py
+++ enigma.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request
 from jinja2 import Template, Environment, FileSystemLoader
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='static',
+            static_url_path='/static',
+            template_folder='templates')
 # file_loader = FileSystemLoader('templates')
 # env = Environment(loader=file_loader)
 
 @app.route('/', methods=["GET", "POST"])
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    return render_template('mainpage.html')
     # return env.get_template('login.html')
 
 if __name__ == '__main__':
