7ef5664fa3c988e446e0f5621e5b7d58192143b1
warlock <maciek.czarnota@gmail.com>
2020-01-11 18:52:35 +0100

    dodano komendy do utworzenia klastra cockroacha

---
+++ scripts/cockroachdb.sh
@@ -0,0 +1,56 @@
+#źródło:
+#https://www.cockroachlabs.com/docs/stable/start-a-local-cluster-in-docker-mac.html
+
+# pobranie obrazu dockera do lokalnego systemu
+sudo docker pull cockroachdb/cockroach:v19.2.2
+# stworzenie sieci dockerowej dla cochroachDB
+sudo docker network create -d bridge roachnet
+# wystartowanie klastra cockroacha, wyjaśnienie flag i parametrów w źródłach
+sudo docker run -d \
+--name=roach1 \
+--hostname=roach1 \
+--net=roachnet \
+-p 26257:26257 -p 8080:8080  \
+-v "${PWD}/cockroach-data/roach1:/cockroach/cockroach-data"  \
+cockroachdb/cockroach:v19.2.2 start \
+--insecure \
+--join=roach1,roach2,roach3
+
+sudo docker run -d \
+--name=roach2 \
+--hostname=roach2 \
+--net=roachnet \
+-v "${PWD}/cockroach-data/roach2:/cockroach/cockroach-data" \
+cockroachdb/cockroach:v19.2.2 start \
+--insecure \
+--join=roach1,roach2,roach3
+
+sudo docker run -d \
+--name=roach3 \
+--hostname=roach3 \
+--net=roachnet \
+-v "${PWD}/cockroach-data/roach3:/cockroach/cockroach-data" \
+cockroachdb/cockroach:v19.2.2 start \
+--insecure \
+--join=roach1,roach2,roach3
+
+# wylistowanie uruchomionych kontenerów
+sudo docker ps
+
+#inicjalizacja bazy danych cockroach
+sudo docker exec -it roach1 ./cockroach init --insecure
+
+# dostep do klienta psql bazy danych
+sudo docker exec -it roach1 ./cockroach sql --insecure
+
+# zastopowanie kontenerów cockroacha
+sudo docker stop roach1 roach2 roach3
+
+# usunięcie kontenerów cockroacha
+sudo docker rm roach1 roach2 roach3
+
+# ponowne wystartowanie kontenerów cockroacha
+sudo docker start roach1 roach2 roach3
+
+# usunięcie danych bazy cockroacha
+rm -rf cockroach-data
